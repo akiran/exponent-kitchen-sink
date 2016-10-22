71a3ba7a4b4d529d8ac8ce7b7a1d342bae201085
kiran <kiran@neostack.com>
2016-10-22 09:23:35 +0530

    Basic facebook login working

--- components/Login.js
+++ components/Login.js
@@ -3,10 +3,28 @@ import {
   StyleSheet,
   Text,
   View,
+  Alert
 } from 'react-native';
+import Exponent from 'exponent'
 import { SocialIcon } from 'react-native-elements'
+import * as config from '../config'
 
 export default class Login extends React.Component {
+  async logIn() {
+    const { type, token } = await Exponent.Facebook.logInWithReadPermissionsAsync(
+      config.FB_APP_ID, {
+        permissions: ['public_profile'],
+      });
+    if (type === 'success') {
+      // Get the user's name using Facebook's Graph API
+      const response = await fetch(
+        `https://graph.facebook.com/me?access_token=${token}`);
+      const name = (await response.json()).name
+      Alert.alert(
+        'Logged in as', name,
+      );
+    }
+  }
   render() {
     return (
       <View style={styles.container}>
@@ -14,6 +32,7 @@ export default class Login extends React.Component {
           title='Sign In With Facebook'
           button
           type='facebook'
+          onPress={this.logIn}
         />
       </View>
     );
